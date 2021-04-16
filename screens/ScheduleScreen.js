f460be1fcd5277282553eb4cc35f4a5c3a170363
zb2450 <zaddeen@gmail.com>
2021-04-16 17:59:56 -0500

    course edit working, mobile styling formik bug

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { StyleSheet, Text, SafeAreaView,} from 'react-native';
 import CourseList from '../components/CourseList';
+import UserContext from '../UserContext';
 
 const schedule = {
   title: "CS Courses for 2020-2021",
@@ -34,10 +35,12 @@ const Banner = ({title}) => (
 );
 
 const ScheduleScreen = ({navigation}) => {
+  const user = useContext(UserContext);
+  const canEdit = user && user.role === 'admin';
   const [schedule, setSchedule] = useState({ title: '', courses: [] });
 
   const view = (course) => {
-    navigation.navigate('CourseDetailScreen', { course });
+    navigation.navigate(canEdit ? 'CourseEditScreen' : 'CourseDetailScreen', { course });
   };
 
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
