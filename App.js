1280fb3c92d626f38c5b496742d860a46313dc61
zb2450 <zaddeen@gmail.com>
2021-04-17 15:36:50 -0500

    authentication fixed

--- App.js
+++ App.js
@@ -1,16 +1,49 @@
 import 'react-native-gesture-handler';
-import React, {useState} from 'react';
+import {firebase} from './utils/firebase';
+import React, {useState, useEffect} from 'react';
+import { Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import ScheduleScreen from './screens/ScheduleScreen';
 import CourseDetailScreen from  './screens/CourseDetailScreen';
 import CourseEditScreen from './screens/CourseEditScreen';
+import RegisterScreen from './screens/RegisterScreen';
 import UserContext from './UserContext';
 
 const Stack = createStackNavigator();
 
+const SignInButton = ({ navigation, user }) => (
+  user && user.uid
+  ? <Button title="Logout" color="#448aff"
+      onPress={() => firebase.auth().signOut()}
+    />
+  : <Button title="SignIn" color="#448aff"
+      onPress={() => navigation.navigate('RegisterScreen')}
+    />
+);
+
 const App = () => {
-  const [user, setUser] = useState({role: 'admin'});
+  const [auth, setAuth] = useState();
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    if (auth && auth.uid) {
+      const db = firebase.database().ref('users').child(auth.uid);
+      const handleData = snap => {
+        setUser({uid: auth.uid, ...snap.val()});
+      }
+      db.on('value', handleData, error => alert(error));
+      return () => { db.off('value', handleData); };
+    } else {
+      setUser(null);
+    }
+  }, [auth]);
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((auth) => {
+      setAuth(auth);
+    });
+  }, []);
 
   return (
     <UserContext.Provider value={user}>
@@ -18,7 +51,12 @@ const App = () => {
         <Stack.Navigator> 
           <Stack.Screen name="ScheduleScreen"
             component={ScheduleScreen}
-            options={{ title: 'Schedule'}} 
+            options={({navigation}) => ({ 
+              title: "Schedule",
+              headerRight: () => (
+              <SignInButton navigation={navigation} user={user} />
+              ),
+            })}
           />
           <Stack.Screen name="CourseDetailScreen"
             component={CourseDetailScreen}
@@ -28,6 +66,7 @@ const App = () => {
             component={CourseEditScreen}
             options={{ title: 'Course Editor'}} 
           />
+          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </UserContext.Provider>
---
+++ screens/RegisterScreen.js
@@ -0,0 +1,126 @@
+import React, {useState} from 'react';
+import {firebase} from '../utils/firebase';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import Form from '../components/Form';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .required('Please enter a valid email')
+    .email()
+    .label('Email'),
+  password: Yup.string()
+    .required()
+    .min(6, 'Password must have at least 6 characters')
+    .label('Password'),
+  confirm: Yup.string()
+    .oneOf([Yup.ref('password'), null], 'Confirmation password must match password'),
+});
+
+const RegisterScreen = ({navigation}) => {
+  const [signInError, setSignInError] = useState('');
+
+  async function handleOnLogin(values) {
+    const { email, password } = values;
+    setSignInError(null);
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      navigation.navigate('ScheduleScreen');
+    } catch (error) {
+      setSignInError(error.message);
+    }
+  }
+
+  async function handleOnSignUp(values) {
+    const { name, email, password } = values;
+    setSignInError(null);
+    try {
+      const authCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
+      const user = authCredential.user;
+      await user.updateProfile({displayName: name});
+      navigation.navigate('ScheduleScreen');
+    } catch (error) {
+      setSignInError(error.message);
+    }
+  }
+
+  async function handleOnSubmit(values) {
+    return values.confirm ? handleOnSignUp(values) : handleOnLogin(values);
+  }
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <Form
+          initialValues={{
+            email: '',
+            password: '',
+            confirm: '',
+          }}
+          validationSchema={validationSchema}
+          onSubmit={handleOnSubmit}
+        >
+          <Form.Field
+            name="email"
+            leftIcon="email"
+            placeholder="Enter email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            textContentType="emailAddress"
+          />
+          <Form.Field
+            name="password"
+            leftIcon="lock"
+            placeholder="Enter password"
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+            textContentType="password"
+          />
+          <Form.Field
+            name="confirm"
+            leftIcon="lock"
+            placeholder="Confirm password"
+            autoCapitalize="none"
+            autoCorrect={false}
+            secureTextEntry={true}
+            textContentType="password"
+          />
+          <Form.Button title={values => values.confirm ? 'Register' : 'Login'} />
+          {<Form.ErrorMessage error={signInError} visible={true} />}
+        </Form>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#ccccb3'
+  },
+  field: {
+    height: 40,
+    width: 300,
+    padding: 5,
+    backgroundColor: 'white',
+  },
+  fieldContainer: {
+    marginBottom: 20,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+    elevation: 4,
+  },
+  label: {
+    fontWeight: 'bold',
+  }
+});
+
+export default RegisterScreen;
